df62517ba4374f89c42e5d2b3591712b055ad9f8
Danatsko <oleksandrdanatsko@gmail.com>
2025-04-21 21:08:41 +0300

    feat(users-server): add UserClientProfileViewSet class

--- backend/users_server/users/views.py
+++ backend/users_server/users/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from oauth2_provider.contrib.rest_framework import TokenMatchesOASRequirements
+from rest_framework import (
+    status,
+    mixins
+)
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from server.authentication import GatewayAndClientTokenAuthentication
+from server.permissions import GatewayTokenMatchesOASRequirements
+from .serializers import (
+    UserClientProfileWriteSerializer,
+    UserClientProfileReadSerializer
+)
+
+User = get_user_model()
+
+
+class UserClientProfileViewSet(
+    mixins.CreateModelMixin,
+    GenericViewSet
+):
+    queryset = User.objects.all()
+    serializer_class = UserClientProfileWriteSerializer
+    permission_classes = [GatewayTokenMatchesOASRequirements]
+    required_alternate_scopes = {
+        'GET': [["users:own:profile:read"],],
+        'POST': [["users:own:profile:create"],],
+        'PUT': [["users:own:profile:update"],],
+        'PATCH': [["users:own:profile:update"],],
+        'DELETE': [["users:own:profile:delete"],],
+    }
+    required_gateway_alternate_scopes = {
+        'GET': [["svc:users:own:profile:read"],],
+        'POST': [["svc:users:own:profile:create"],],
+        'PUT': [["svc:users:own:profile:update"],],
+        'PATCH': [["svc:users:own:profile:update"],],
+        'DELETE': [["svc:users:own:profile:delete"],],
+    }
+
+    @action(
+        detail=False,
+        methods=['GET', 'PUT', 'PATCH', 'DELETE'],
+        url_path='me',
+        authentication_classes=[GatewayAndClientTokenAuthentication],
+        serializer_class=UserClientProfileWriteSerializer,
+        permission_classes=[
+            TokenMatchesOASRequirements,
+            GatewayTokenMatchesOASRequirements
+        ]
+    )
+    def me(self, request):
+        if request.method == 'GET':
+            serializer = UserClientProfileReadSerializer(request.user)
+            response_data = {'user': serializer.data}
+
+            return Response(response_data)
+        elif request.method in ['PUT', 'PATCH']:
+            serializer = self.get_serializer(request.user, data=request.data, partial=True)
+
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            response_data = {'user': serializer.data}
+
+            return Response(response_data)
+        elif request.method == 'DELETE':
+            request.user.delete()
+
+            return Response(status=status.HTTP_204_NO_CONTENT)
