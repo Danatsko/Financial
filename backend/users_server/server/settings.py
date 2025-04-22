adfdc1c7e606c013f5d675718e4adfa34570f0ed
Danatsko <oleksandrdanatsko@gmail.com>
2025-04-22 20:22:12 +0300

    feat(users-server): add DEFAULT_SCHEMA_CLASS to REST_FRAMEWORK configuration

--- backend/users_server/server/settings.py
+++ backend/users_server/server/settings.py
@@ -167,6 +167,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 GATEWAY_APPLICATION_CLIENT_IDS = env.list('GATEWAY_APPLICATION_CLIENT_IDS')
