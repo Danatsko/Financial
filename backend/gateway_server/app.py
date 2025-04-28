02cb476cd9879bb50b9add05d0788a80261c4dbb
Danatsko <oleksandrdanatsko@gmail.com>
2025-04-29 00:56:16 +0300

    feat(gateway-server): implement app lifespan for resource management

--- backend/gateway_server/app.py
+++ backend/gateway_server/app.py
@@ -1,14 +1,61 @@
+from contextlib import asynccontextmanager
+
 from fastapi import (
     FastAPI,
     APIRouter
 )
 
+from connections.redis_connection_managers import RedisConnectionManager
+from connections.upstream_connection_managers import UpstreamConnectionManager
 from exceptions.exception_handlers import gateway_exception_handler
 from exceptions.exceptions import GatewayException
+from settings import settings
 from transactions.routes import transactions_router
 from users.routes import users_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+    users_connection_manager = UpstreamConnectionManager(
+        manager_name="Users manager connections",
+        client_id=settings.users_server_gateway_application_client_id,
+        client_secret=settings.users_server_gateway_application_client_secret,
+        base_url=str(settings.users_server_url),
+        token_endpoint=f"{settings.users_server_url}/o/token/",
+        scope=settings.users_server_gateway_scopes
+    )
+
+    transactions_connection_manager = UpstreamConnectionManager(
+        manager_name="Transactions manager connections",
+        client_id=settings.transactions_server_gateway_application_client_id,
+        client_secret=settings.transactions_server_gateway_application_client_secret,
+        base_url=str(settings.transactions_server_url),
+        token_endpoint=f"{settings.transactions_server_url}/o/token/",
+        scope=settings.transactions_server_gateway_scopes
+    )
+
+    redis_connection_manager = RedisConnectionManager(
+        host=settings.redis_server_host,
+        port=settings.redis_server_port,
+        db=settings.redis_server_db
+    )
+
+    app.state.users_connection_manager = users_connection_manager
+    app.state.transactions_connection_manager = transactions_connection_manager
+    app.state.redis_connection_manager = redis_connection_manager
+
+    await users_connection_manager.start()
+    await transactions_connection_manager.start()
+    await redis_connection_manager.start()
+
+    try:
+        yield
+    finally:
+        await users_connection_manager.shutdown()
+        await transactions_connection_manager.shutdown()
+        await redis_connection_manager.shutdown()
+
+app = FastAPI(lifespan=app_lifespan)
 
 app.add_exception_handler(GatewayException, gateway_exception_handler)
 
