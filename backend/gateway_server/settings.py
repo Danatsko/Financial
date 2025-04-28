8cc4adafdd419527f9972779af37700d3c067362
Danatsko <oleksandrdanatsko@gmail.com>
2025-04-29 00:53:12 +0300

    feat(gateway-server): update application settings for services

--- backend/gateway_server/settings.py
+++ backend/gateway_server/settings.py
@@ -13,10 +13,13 @@ class Settings(BaseSettings):
     users_server_gateway_application_client_secret: str
     users_server_users_application_client_id: str
     users_server_users_application_client_secret: str
+    users_server_gateway_scopes: str
+    users_server_users_scopes: str
 
     transactions_server_url: AnyHttpUrl
     transactions_server_gateway_application_client_id: str
     transactions_server_gateway_application_client_secret: str
+    transactions_server_gateway_scopes: str
 
     redis_server_host: str
     redis_server_port: int
