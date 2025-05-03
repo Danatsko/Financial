43a3908a366886c807deb8d18386b3dc40550a25
KeeRReeK <valentinreek@gmail.com>
2025-05-03 17:41:31 +0300

    feat(frontend): initial commit

---
+++ frontend/Financial/Financial/FinancialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialApp.swift
+//  Financial
+//
+//  Created by Анатолій Ганулич on 03.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FinancialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
