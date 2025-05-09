6c9aa402b08125b784ae209f940cc82b684ea730
KeeRReeK <valentinreek@gmail.com>
2025-05-09 18:35:23 +0300

    feat(frontend): add functional in Transaction List View

--- frontend/Financial/Financial/View/TransactionListView.swift
+++ frontend/Financial/Financial/View/TransactionListView.swift
@@ -7,12 +7,53 @@
 
 import SwiftUI
 
-struct TransactionListView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+enum TransactionRoute: Hashable {
+    case detail(Transaction)
+    case edit(Transaction)
 }
 
-#Preview {
-    TransactionListView()
+struct TransactionListView: View {
+    
+    var coreDataService = CoreDataManager.shared
+    @State private var path = NavigationPath()
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    @FetchRequest(
+        sortDescriptors: [],
+        predicate: NSPredicate(format: "email != nil"),
+        animation: .default
+    ) private var user: FetchedResults<User>
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            
+            HStack {
+                Text("yourBalance")
+                    .font(.custom("Montserrat-Bold", size: 24))
+                    .foregroundColor(.white)
+                Spacer()
+            }
+            
+            if let currentUser = user.first {
+                BalanceCardView(user: currentUser)
+            }
+            
+            HStack {
+                Text("transactions")
+                    .font(.custom("Montserrat-Bold", size: 24))
+                    .foregroundColor(.white)
+                
+                Spacer()
+            }
+            
+            TransactionListViewContent(path: $path)
+        }
+        .padding()
+        .background(Color("BackroundColor"))
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onTapGesture {
+            UIApplication.shared.dismissKeyboard()
+        }
+    }
 }
