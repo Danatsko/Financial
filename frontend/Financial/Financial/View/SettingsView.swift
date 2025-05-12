6c96bf8296f8a557faad1a735353cfaaf6db204b
KeeRReeK <valentinreek@gmail.com>
2025-05-12 13:12:33 +0300

    feat(frontend): add settings view

---
+++ frontend/Financial/Financial/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Financial
+//
+//  Created by KeeR ReeK on 12.05.2025.
+//  Copyright (c) 2025 Financial
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
