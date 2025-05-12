307663e0c6021fbfb7f1aaaa17615a5e0cb0961c
Danatsko <oleksandrdanatsko@gmail.com>
2025-05-12 18:03:49 +0300

    feat(gateway-server): add ProviderPath and PostSocialExchangeTokenRequestParamsBody annotations

--- backend/gateway_server/users/annotations.py
+++ backend/gateway_server/users/annotations.py
@@ -3,17 +3,21 @@ from typing import Annotated
 from fastapi import (
     Body,
     Header,
+    Path
 )
 
 from .models import (
     PatchMeRequest,
     PostRegistrationRequest,
     PostLoginRequest,
-    PostRefreshTokenRequest
+    PostRefreshTokenRequest,
+    PostSocialExchangeTokenRequest
 )
 
+ProviderPath = Annotated[str, Path(title='The name of the social provider.')]
 AuthHeader = Annotated[str, Header(title='The user authorization token.', alias='Authorization')]
 PatchMeRequestBody = Annotated[PatchMeRequest, Body(title='Data to update the user.')]
 PostRegistrationRequestBody = Annotated[PostRegistrationRequest, Body(title='Data to register the user.')]
 PostLoginRequestBody = Annotated[PostLoginRequest, Body(title='Data to login the user.')]
 PostRefreshTokenRequestBody = Annotated[PostRefreshTokenRequest, Body(title='Data to refresh the token.')]
+PostSocialExchangeTokenRequestParamsBody = Annotated[PostSocialExchangeTokenRequest, Body(title='Parameters for social token exchange.')]
