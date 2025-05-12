b6a3721e57246d8c0632ba1ac5dfbcbcb8eb4e23
Danatsko <oleksandrdanatsko@gmail.com>
2025-05-12 16:37:27 +0300

    feat(users-server): add SocialTokenExchangeAPIView

--- backend/users_server/users/views.py
+++ backend/users_server/users/views.py
@@ -2,10 +2,22 @@ import uuid
 
 from allauth.account.adapter import get_adapter
 from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.utils.timezone import (
+    now,
+    timedelta
+)
 from django.http import JsonResponse
 from django.core.cache import cache
 from django.shortcuts import redirect
 from oauth2_provider.contrib.rest_framework import TokenMatchesOASRequirements
+from oauth2_provider.models import (
+    get_application_model,
+    AccessToken,
+    RefreshToken
+)
+from oauth2_provider.settings import oauth2_settings
+from oauthlib.common import generate_token
 from rest_framework import (
     status,
     mixins
@@ -28,6 +40,7 @@ from .serializers import (
 )
 
 User = get_user_model()
+Application = get_application_model()
 
 
 class UserClientProfileViewSet(
@@ -133,3 +146,75 @@ class GenerateAppCodeAPIView(APIView):
         client_callback_url_with_code = f'myapp://social-login-callback?app_code={app_code}&provider={provider}'
 
         return redirect(client_callback_url_with_code)
+
+
+class SocialTokenExchangeAPIView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(
+            self,
+            request,
+            *args,
+            **kwargs
+    ):
+        app_code = request.data.get('app_code')
+
+        if not app_code:
+            return Response(
+                {'error': 'Application code is required.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user_id = cache.get(f'django:auth:app_code:{app_code}')
+
+        if not user_id:
+            return Response(
+                {'error': 'Invalid or expired application code.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        cache.delete(f'django:auth:app_code:{app_code}')
+
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response(
+                {'error': 'User not found for this code.'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        try:
+            application = Application.objects.get(client_id=settings.SOCIALACCOUNT_APPLICATION_CLIENT_ID)
+        except Application.DoesNotExist:
+            return Response(
+                {'error': 'OAuth2 application for token generation not configured on Django server.'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+        expires = now() + timedelta(seconds=oauth2_settings.ACCESS_TOKEN_EXPIRE_SECONDS)
+        access_token = AccessToken(
+            user=user,
+            application=application,
+            token=generate_token(),
+            expires=expires,
+            scope="users:own:profile:create users:own:profile:read users:own:profile:update users:own:profile:delete achievements:own:list:read achievements:own:item:update"
+        )
+
+        access_token.save()
+
+        refresh_token = RefreshToken(
+            user=user,
+            application=application,
+            token=generate_token(),
+            access_token=access_token
+        )
+
+        refresh_token.save()
+
+        return Response({
+            'access_token': access_token.token,
+            'expires_in': oauth2_settings.ACCESS_TOKEN_EXPIRE_SECONDS,
+            'token_type': 'Bearer',
+            'scope': access_token.scope,
+            'refresh_token': refresh_token.token
+        })
