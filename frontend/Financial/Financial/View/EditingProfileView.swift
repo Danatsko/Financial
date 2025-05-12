1139868a1dd99de0734e4ddaff80915116de50b6
KeeRReeK <valentinreek@gmail.com>
2025-05-12 13:15:21 +0300

    feat(frontend): add Editing Profile View Model

---
+++ frontend/Financial/Financial/View/EditingProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditingProfileView.swift
+//  Financial
+//
+//  Created by KeeR ReeK on 12.05.2025.
+//
+
+import SwiftUI
+
+struct EditingProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditingProfileView()
+}
---
+++ frontend/Financial/Financial/ViewModel/EditingProfileViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  EditingProfileViewModel.swift
+//  Financial
+//
+//  Created by KeeR ReeK on 12.05.2025.
+//  Copyright (c) 2025 Financial
+
+import Foundation
+
+class EditingProfileViewViewModel: ObservableObject {
+    
+    let availableImages = ["firstBoy", "secondBoy", "thirdBoy", "firstGirl", "secondGirl", "thirdGirl"]
+    
+}
