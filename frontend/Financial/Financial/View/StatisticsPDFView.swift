5200ab84ffa0155024cfef53ead9948d8a1df16e
KeeRReeK <valentinreek@gmail.com>
2025-05-27 16:52:13 +0300

    feat(frontend): add logo

---
+++ frontend/Financial/Financial/View/StatisticsPDFView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsPDFView.swift
+//  Financial
+//
+//  Created by KeeR ReeK on 24.05.2025.
+//
+
+import SwiftUI
+
+struct StatisticsPDFView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticsPDFView()
+}
